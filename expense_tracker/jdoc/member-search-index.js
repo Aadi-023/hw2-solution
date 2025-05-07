9c5a5279d2561e315a6fe31379643fa5372dbc8f
Aadii <adeshmukh@umass.edu>
2025-05-06 21:59:13 -0400

    test

--- expense_tracker/jdoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"ExpenseTrackerApp","l":"ExpenseTrackerApp()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ExpenseTrackerApp","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
